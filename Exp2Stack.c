d0351ce22d49923bd5dab97c83c1c7bb925e1a8f
Ashutosh-tech736 <ashutoshdubey9880@gmail.com>
2026-07-15 15:19:11 +0530

    Add files via upload

---
+++ Exp2Stack.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+{
+    void main()
+    printf("Enter the Number of Elements ");
+    scanf("%d",&n);
+     int top=-1;
+     if(top==(Max-1))
+     {
+        printf("Stack is full");
+     }
+     else
+     top++;
+     Stack[top]=i;
+           Stack[top] = n;   // Stores the entered number
+        printf("Element %d pushed successfully.\n", Stack[top]);
+    }
+
+    return 0;
+
+}
