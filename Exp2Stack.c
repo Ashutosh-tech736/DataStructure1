bda3c42a0b538f966c60fffd0e2ec5c1ca87fd16
Ashutosh-tech736 <ashutoshdubey9880@gmail.com>
2026-07-15 15:22:52 +0530

    Update Exp2Stack.c

--- Exp2Stack.c
+++ Exp2Stack.c
@@ -1,21 +1,24 @@
 #include<stdio.h>
-#include<conio.h>
+#define Max 10
+int main()
 {
-    void main()
+    int n;
+    int Stack[Max];
+    int top = -1;
+    int i;
     printf("Enter the Number of Elements ");
     scanf("%d",&n);
-     int top=-1;
+     
      if(top==(Max-1))
      {
         printf("Stack is full");
      }
      else
      top++;
-     Stack[top]=i;
-           Stack[top] = n;   // Stores the entered number
-        printf("Element %d pushed successfully.\n", Stack[top]);
+    Stack[top] = n;   // Stores the entered number
+    printf("Element %d pushed successfully.\n", Stack[top]);
     }
 
-    return 0;
+   
 
 }
